82f5bde0e02295167833e6df1df46dfbf70f65ab
Abir-Hasan-52 <abirhasan5208+github@gmail.com>
2025-04-09 19:02:38 +0600

    lift up the state to handle child component event

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -1,21 +1,29 @@
-import React, { use } from 'react';
-import Country from '../Country/Country';
-import './Countries.css';
+import React, { use, useState } from "react";
+import Country from "../Country/Country";
+import "./Countries.css";
 
-const Countries = ({countriesPromise}) => {
-    const countries= use(countriesPromise);
-    // console.log(countries);
-    return (
-        <div >
-            <h1>Traveling countries : {countries.length}</h1>
-            <div className='countries'>
-                {
-                countries.map(country =><Country key={country.cca3}  country={country}></Country>)
-            
-            }
-            </div>
-        </div>
-    );
+const Countries = ({ countriesPromise }) => {
+  const [visitedCountries, setVisitedCountries] = useState([]);
+  const countries = use(countriesPromise);
+  // console.log(countries);
+
+  const handleVisitedCountries = (country) => {
+    console.log('visited countries:',country);
+  };
+  return (
+    <div>
+      <h1>Traveling countries : {countries.length}</h1>
+      <h3>Travels so fer:</h3>
+      <div className="countries">
+        {countries.map((country) => (
+          <Country
+           key={country.cca3}
+          handleVisitedCountries={handleVisitedCountries}
+          country={country}></Country>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Countries;
