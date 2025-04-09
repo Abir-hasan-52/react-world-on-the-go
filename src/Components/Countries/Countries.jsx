2c56eb16d450ab7d8f43ab09ba1964b366d67a21
Abir-Hasan-52 <abirhasan5208+github@gmail.com>
2025-04-09 09:39:42 +0600

    countries data te css apply

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -1,16 +1,19 @@
 import React, { use } from 'react';
 import Country from '../Country/Country';
+import './Countries.css';
 
 const Countries = ({countriesPromise}) => {
     const countries= use(countriesPromise);
     console.log(countries);
     return (
-        <div>
+        <div >
             <h1>Traveling countries : {countries.length}</h1>
-            {
+            <div className='countries'>
+                {
                 countries.map(country =><Country key={country.cca3}  country={country}></Country>)
             
             }
+            </div>
         </div>
     );
 };
--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Country.css';
 
 const Country = ({country}) => {
     // console.log(country);
     return (
-        <div>
+        <div className='country'>
             <h3>Name:{country.name.common}</h3>
             <img src={country.flags.png} alt="" />
             <p>independent: {country.independent ? 'free': 'not free'}</p>
